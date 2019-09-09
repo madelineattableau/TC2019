0532cf6f561ed152692a08413b8b6236dd60fb63
Madeline Lee <54322412+madelinefromtableau@users.noreply.github.com>
2019-09-09 09:28:27 -0400

    fixed setting with copy warning code

--- yelp_prep_solution.py
+++ yelp_prep_solution.py
@@ -11,11 +11,15 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 def classify_sentiment(data_frame):
     #for loop to go through each line of text in dataframe
+    sentiment_list = []
     for i in range(0, data_frame.shape[0]):
         #run polarity scoring
         score = SentimentIntensityAnalyzer().polarity_scores(data_frame['text'].iloc[i])
-        #assign to sentiment column
-        data_frame['sentiment'].iloc[i] = score['compound']
+        #append to sentiment_list
+        sentiment_list.append(score['compound'])
+
+    #add back to dataframe
+    data_frame['sentiment'] = sentiment_list
     #return data frame
     return(data_frame)
     
--- yelp_prep_starter.py
+++ yelp_prep_starter.py
@@ -1,12 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Sep  9 08:55:56 2019
-
-@author: madlee
-"""
-
-# -*- coding: utf-8 -*-
-"""
 Created on Tue Sep  3 10:46:49 2019
 
 @author: madlee
@@ -19,11 +12,15 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 #function to classify sentiment
 def classify_sentiment(data_frame):
     #for loop to go through each line of text in dataframe
+    sentiment_list = []
     for i in range(0, data_frame.shape[0]):
         #run polarity scoring
         score = SentimentIntensityAnalyzer().polarity_scores(data_frame['xxx'].iloc[i])
-        #assign desired type of sentiment score to sentiment column
-        data_frame['sentiment'].iloc[i] = score['xxx']
+        #append desired type of sentiment score to sentiment_list
+        sentiment_list.append(score['xxx'])
+
+    #reassign to sentiment column
+    data_frame['xxx'] = xxx
     #return data frame
     return(data_frame)
     
